8824535b06b134f1830698888de14275bf4c4667
Pavel Akatev <pavel_akatev@epam.com>
2022-01-15 15:31:44 +0300

    добавил библиотеку net standart 2/0

---
+++ 2Introduction .net/MultitargetingStandart2.0/StringWithTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MultitargetingStandart2._0
+{
+    public class StringWithTime
+    {
+        DateTime dateTime { get; set; }
+
+        public string OutputNameDatetime(string name)
+        {
+            dateTime = DateTime.Now;
+            string output = $"{dateTime} Hello,{name}!";
+            return output;
+        }
+    }
+}
