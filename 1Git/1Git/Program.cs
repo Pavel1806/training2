f7d72a584b5c3bd6d3f05e2d5dee81f0c7bf66b8
Pavel Akatev <pavel_akatev@epam.com>
2022-01-20 17:46:06 +0300

    Сделал отдельный sln для git(Git)

---
+++ 1Git/1Git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 1;
+            bool bugfix = true;
+            bool bugfix0 = false;
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
