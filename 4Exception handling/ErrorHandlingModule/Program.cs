90bf274d5375ab630ab741e7f9f7acb67b51f8cc
Pavel Akatev <pavel_akatev@epam.com>
2022-02-02 13:16:54 +0300

    [Exception handling] Выведение первой буквы слова, обработка и выведение ошибки в консоль

---
+++ 4Exception handling/ErrorHandlingModule/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ErrorHandlingModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            var wordConsole = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(wordConsole[0]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+
+        }
+    }
+}
