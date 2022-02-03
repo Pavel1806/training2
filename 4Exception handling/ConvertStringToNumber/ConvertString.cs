13ef6ec5505d90767b5e32339d964179e81a9b8d
Pavel Akatev <pavel_akatev@epam.com>
2022-02-03 09:35:28 +0300

    [Exception handling] Версия 1.

---
+++ 4Exception handling/ConvertStringToNumber/ConvertString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace ConvertStringToNumber
+{
+    public static class ConvertString
+    {
+        public static int ToInt(string wordConvert)
+        {
+            int a;
+            int[] arrayInt = new int[wordConvert.Length];
+            
+            for(int i = 0 ; i< wordConvert.Length; i++)
+            {
+                char characterFromString = wordConvert[i];
+                a = characterFromString - '0';
+                if (a <= 9)
+                {
+                    arrayInt[i] = a;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            int number = 0;
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                number += arrayInt[i] * (int)Math.Pow(10, arrayInt.Length - 1 - i);
+            }
+
+            return number;
+
+            
+
+        }
+    }
+}
--- 4Exception handling/ErrorHandlingModule/Program.cs
+++ 4Exception handling/ErrorHandlingModule/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConvertStringToNumber;
+using System;
 
 namespace ErrorHandlingModule
 {
@@ -18,6 +19,9 @@ namespace ErrorHandlingModule
                 Console.WriteLine(e.Message); 
             }
 
+            int t =  ConvertString.ToInt("eruut");
+
+            Console.WriteLine(t);
         }
     }
 }
