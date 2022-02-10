f9b65da702a6ac3fa7f0a27662d11a5cd38d8afe
Pavel Akatev <pavel_akatev@epam.com>
2022-02-10 12:46:14 +0300

    [LINQ] Выполненs 2, 3, 4, 5

--- 5LINQ/Task/Task/LinqSamples.cs
+++ 5LINQ/Task/Task/LinqSamples.cs
@@ -25,7 +25,7 @@ namespace SampleQueries
 
 		private DataSource dataSource = new DataSource();
 
-		[Category("Restriction Operators")]
+		[Category("Task 1")]
 		[Title("Where - Task 1")]
 		[Description("Список клиентов у которых сумма всех заказов больше orderAmmount")]
 		public void Linq1()
@@ -34,28 +34,154 @@ namespace SampleQueries
             
             var listOfSortedClients = dataSource.Customers.Where(x => x.Orders.Sum(y => y.Total) > orderAmmount);
 
-            foreach (var x in listOfSortedClients)
+            foreach (var item in listOfSortedClients)
             {
-                Console.WriteLine(x.CompanyName);
+                Console.WriteLine(item.CompanyName);
             }
         }
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
+		[Category("Task 2")]
+		[Title("Where - Task 2.1")]
+		[Description("Список поставщиков, находящихся в той же стране и том же городе")]
 
-		public void Linq2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+		public void Linq2_1()
+        {
+            var listCustomer = dataSource.Customers;
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
+            foreach (var item in listCustomer)
+            {
+                var listOfSortedSuppliers = dataSource.Suppliers.Where(x => x.Country == item.Country).Where(y => y.City == item.City);
+                
+                Console.WriteLine();
+                Console.WriteLine($"Имя клиента {item.CompanyName}");
+                Console.WriteLine();
 
-	}
+                if (listOfSortedSuppliers.Count() != 0)
+                {
+                    Console.WriteLine("Список поставщиков");
+                    foreach (var x in listOfSortedSuppliers)
+                    {
+                        Console.WriteLine(x.SupplierName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Поставщиков в той же стране и в том же городе, что и клиент, не найдено");
+                }
+            }
+        }
+
+        [Category("Task 2")]
+        [Title("Where - Task 2.2")]
+        [Description("Список поставщиков, находящихся в той же стране и том же городе")]
+
+        public void Linq2_2()
+        {
+          
+            var listOfSortedCustomers = 
+                dataSource.Customers.GroupJoin(
+                dataSource.Suppliers, 
+                t => new {t.Country, t.City}, 
+                p => new { p.Country, p.City },
+                (custom, spls) => new 
+                {
+                   Name = custom.CompanyName,
+                   Suppliers = spls.Select(p=>p.SupplierName)
+                });
+            
+            foreach (var item in listOfSortedCustomers)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine($"Имя клиента {item.Name}");
+                Console.WriteLine();
+                
+                if (item.Suppliers.Count() != 0)
+                {
+                    Console.WriteLine("Список поставщиков находящихся в том же городе");
+                    foreach (var x in item.Suppliers)
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Поставщиков в том же городе, что и клиент, не найдено");
+                }
+
+            }
+
+
+        }
+        [Category("Task 3")]
+        [Title("Where - Task 3")]
+        [Description("Все клиенты, у которых были заказы, превосходящие по сумме величину X")]
+
+        public void Linq3()
+        {
+            var listOfSortedCustomers = dataSource.Customers.Where(x => x.Orders.Any(y=>y.Total > 5544));
+
+            foreach (var item in listOfSortedCustomers)
+            {
+                Console.WriteLine(item.CompanyName);
+            }
+        }
+
+        [Category("Task 4")]
+        [Title("Where - Task 4.1")]
+        [Description("Список клиентов с указанием, начиная с какого месяца какого года они стали клиентами")]
+        public void Linq4_1()
+        {
+            var listOfSortedCustomers = dataSource.Customers.Select(x => new
+            {
+                Customer = x.CompanyName,
+                Date = x.Orders.Length != 0 ? x.Orders.Min(s => s.OrderDate) : new DateTime()
+            });
+
+            foreach (var item in listOfSortedCustomers)
+            {
+                Console.WriteLine($"{item.Customer}--{item.Date}");
+            }
+        }
+
+        [Category("Task 4")]
+        [Title("Where - Task 4.2")]
+        [Description("Список клиентов с указанием, начиная с какого месяца какого года они стали клиентами")]
+        public void Linq4_2()
+        {
+            foreach (var item in dataSource.Customers)
+            {
+                Console.WriteLine();
+                Console.WriteLine(item.CompanyName);
+                Console.WriteLine();
+                if (item.Orders.Length == 0)
+                {
+                    Console.WriteLine("Еще не покупал ничего");
+                }
+                else
+                {
+                    Console.WriteLine(item.Orders.Min(s => s.OrderDate));
+                }
+            }
+        }
+
+        [Category("Task 5")]
+        [Title("Where - Task 5")]
+        [Description("Список отсортированных по году, месяцу, оборотам клиента (от максимального к минимальному) и имени клиента")]
+        public void Linq5()
+        {
+
+            var listOfSortedCustomers = dataSource.Customers.Select(x => new
+            {
+                Customer = x.CompanyName,
+                moneyTurnover = x.Orders.Sum(y=>y.Total),
+                Date = x.Orders.Length != 0 ? x.Orders.Min(s => s.OrderDate) : new DateTime()
+            }).OrderByDescending(x=>x.Date.Year).ThenByDescending(t=>t.Date.Month).ThenByDescending(j=>j.moneyTurnover);
+
+            foreach (var item in listOfSortedCustomers)
+            {
+                Console.WriteLine($"{item.Date.Year}--{item.Date.Month}--{item.moneyTurnover}--{item.Customer}");
+            }
+        }
+    }
 }
