e18ab7ccd32a40e788620e3c714740b9b79ca692
Pavel Akatev <pavel_akatev@epam.com>
2022-04-01 17:15:23 +0300

    [SQL] Доделанные задания 1.3, сделанные 1.4

--- 9SQL/1_3.sql
+++ 9SQL/1_3.sql
@@ -7,3 +7,15 @@ SELECT CustomerID, Country
 FROM Customers
 WHERE Country BETWEEN 'b' AND 'h'
 ORDER BY Country
+
+
+
+SELECT CustomerID, Country
+FROM Customers
+WHERE Country > 'b' AND Country < 'h'
+ORDER BY Country
+
+
+SELECT ProductName
+FROM Products
+WHERE ProductName LIKE 'cho_olade'
