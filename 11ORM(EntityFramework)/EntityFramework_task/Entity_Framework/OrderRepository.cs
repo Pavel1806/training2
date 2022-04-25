ff6925718be175726a343988a1a72ced99302198
Pavel Akatev <pavel_akatev@epam.com>
2022-04-25 19:10:09 +0300

    [Orm] Начало

---
+++ 11ORM(EntityFramework)/EntityFramework_task/Entity_Framework/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Entity_Framework.Context;
+using Entity_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity_Framework
+{
+    class OrderRepository : IOrderRepository
+    {
+        public void GetOrderByCategory()
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            var r = context.OrderDetails.AsEnumerable().GroupBy(x=>x.OrderId).ToList();
+
+            var p = context.Orders.Join(context.OrderDetails, p => p.OrderId, c => c.OrderId,
+                (p, c) => new
+                {
+                    Category = c.Product.Category.CategoryId,
+                    Product = c.Product.ProductName,
+                    OrderId = p.OrderId,
+                    OrderDetail = p.OrderDetails
+                }).Where(c => c.Category == 1).AsEnumerable().GroupBy(x => x.OrderId).Select(c=> new { 
+                           OrderId = c.Key,
+                           OrderDetails = c.Select(p=>p)
+                }).ToList();
+
+            var t = context.Orders.Join(context.OrderDetails, p => p.OrderId, c=>c.OrderId, 
+                (p, c) => new { 
+                    Category = c.Product.Category.CategoryId,
+                    Product = c.Product.ProductName,
+                    OrderId= p.OrderId
+                }).ToList();
+
+            foreach(var orderId in p)
+            {
+                Console.WriteLine($"{orderId.OrderId}");
+
+                foreach (var item in orderId.OrderDetails)
+                {
+                    Console.WriteLine($"{item.Product}--{item.Category}");
+                }
+            }
+        }
+    }
+}
---
+++ 11ORM(EntityFramework)/EntityFramework_task/Entity_Framework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Entity_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrderRepository repository = new OrderRepository();
+
+            repository.GetOrderByCategory();
+        }
+    }
+}
