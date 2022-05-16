b964e3b2fa52d58b586e9b2aa3ca49c6855cf6f0
Pavel Akatev <pavel_akatev@epam.com>
2022-05-16 18:14:45 +0300

    [Http] Создал папку задания. Накидал пару методов.

---
+++ 13HTTP/СopyingSiteSolution/SiteFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using СopyingSite;
+
+namespace SiteFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopySite cs = new CopySite("https://www.mebelion.ru/");
+
+            cs.Copy();
+        }
+    }
+}
---
+++ 13HTTP/СopyingSiteSolution/СopyingSite/CopySite.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace СopyingSite
+{
+    public class CopySite
+    {
+        private string link;
+
+        public CopySite(string link)
+        {
+            this.link = link;
+        }
+
+        public void Copy()
+        {
+            using (var handler = new HttpClientHandler())
+            using(var client = new HttpClient(handler))
+            {
+                var file = new FileStream("image.html", FileMode.Create);
+
+                var t = client.GetAsync(link);
+
+                var y = t.Result.Content.ReadAsStreamAsync();
+
+                y.Result.CopyTo(file);  
+            }
+        }
+
+        public IEnumerable<string> HtmlAgilityPack()
+        {
+            HtmlDocument htmlSnippet = new HtmlDocument();
+            htmlSnippet.Load("D:\\VisualStudio\\repos\\training\\13HTTP\\СopyingSiteSolution\\SiteFolder\\bin\\Debug\\netcoreapp3.1\\image.html");
+
+            List<string> hrefTags = new List<string>();
+
+            var t = htmlSnippet.DocumentNode.SelectNodes("//a[@href]");
+
+            foreach (HtmlNode link in t)
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                hrefTags.Add(att.Value);
+            }
+
+            return hrefTags;
+        }
+    }
+}
